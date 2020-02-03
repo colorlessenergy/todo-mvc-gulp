31e62820f55c2191a9877b6fca8536301a949155
Brian Munoz <brianmunozdev@gmail.com>
2020-02-03 09:39:11 -0800

    setup base files to convert sass and bundle js files with gulp

---
+++ gulpfile.js
@@ -0,0 +1,22 @@
+const gulp = require('gulp');
+const sass = require('gulp-sass');
+const concat = require('gulp-concat');
+
+gulp.task('styles', function() {
+  return gulp.src('sass/**/*.scss')
+    .pipe(sass().on('error', sass.logError))
+    .pipe(gulp.dest('./build/css/'))
+});
+
+gulp.task('concat', function () {
+  return gulp.src(['js/models/*.js', 'js/controllers/*.js', 'js/views/*.js'])
+    .pipe(concat('main.js'))
+    .pipe(gulp.dest('./build/js/'))
+});
+
+gulp.task('watch', function () {
+  gulp.watch('sass/**/*.scss', gulp.series(['styles']));
+  gulp.watch('js/**/*.js', gulp.series(['concat']));
+});
+
+gulp.task('default', gulp.series(['styles', 'concat']))
---
+++ js/models/todoModel.js
@@ -0,0 +1,6 @@
+const todoModel = [
+  {
+    id: 0,
+    value: 'this is a todo'
+  }
+];
