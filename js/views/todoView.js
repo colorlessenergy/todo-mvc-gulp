836333668e0533b70df56c606045475b82e13f3e
colorlessenergy <brianmunozdev@gmail.com>
2020-02-03 14:02:57 -0800

    add functionality to add and look at todos

--- js/views/todoView.js
+++ js/views/todoView.js
@@ -1,1 +1,29 @@
-console.log('this is the todo view');
+(function () {
+  const form = document.querySelector('#todoForm');
+  
+  
+  form.addEventListener('submit', function (ev) {
+    ev.preventDefault();
+    todoController.createTodo(form.todo.value);
+    displayTodos();
+
+    form.todo.value = '';
+  });
+  
+  
+  
+  function displayTodos () {
+    let todosContainer = document.querySelector('#todos');
+    let todos = todoController.getAllTodos();
+    
+    todosContainer.innerHTML = '';
+    todos.forEach(function (todo) {
+      let createP = document.createElement('p');
+      createP.textContent = todo.value;
+      todosContainer.appendChild(createP);
+    });
+  }
+
+
+  displayTodos();
+})()
